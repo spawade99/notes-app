ab10a8905556adbee3fe17911440b7e77fcc94ae
Shubham <51116166+spawade99@users.noreply.github.com>
2024-07-26 22:56:28 +0530

    Basic Setup (#2)
    Fix this ui in future commit and add drag and drop support.

---
+++ src/pages/NotesPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NoteCard from '../components/NoteCard';
+import { note } from '../assets/fakeData';
+type NotesPageProps = {
+    notes: note[];
+};
+const NotesPage: React.FC<NotesPageProps> = ({ notes }) => {
+
+    return (
+        <>
+            {
+                notes.map((note, index) => (
+                    <NoteCard key={index} note={note} />
+                ))
+            }
+        </>
+    );
+}
+export default NotesPage;
