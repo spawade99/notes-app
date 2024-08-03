3258bf930f5c738d3adba143b0c2457b66e8c18c
Shubham Pawade <sspawade333@gmail.com>
2024-08-03 14:17:34 +0530

    configured own deployment setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import NotesPage from "./pages/NotesPage"
 function App() {
   return (
     <div id={"app"}>
+      <h1>
+        This is a test deployement of a React app on Github Pages
+      </h1>
+
       <NotesPage />
     </div>
   )
